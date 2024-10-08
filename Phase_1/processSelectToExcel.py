f6edcc377fb7d15866203e0aed81ca5dd829d8ae
George Pearson <gpearson31@gatech.edu>
2024-10-08 08:35:16 -0400

    got rid of mappings.json
    i realized i dont actually need it, will update the readme and maybe some other files next

--- Phase_1/processSelectToExcel.py
+++ Phase_1/processSelectToExcel.py
@@ -119,7 +119,7 @@ def process_files(folder_path, output_excel_file):
             print(f)
 
 if __name__ == "__main__":
-    folder_path = r'text_files\2020-04'
+    folder_path = r'text_files\2020-05'
     #folder_path = 'testing_files'
     output_excel_file = 'select_race_data.xlsx'
     process_files(folder_path, output_excel_file)
