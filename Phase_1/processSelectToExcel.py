7ed636b0a4df934d32450ce591d1a2dab1cca541
George Pearson <gpearson31@gatech.edu>
2024-10-11 11:01:43 -0400

    cleaned and checked Jan 2021 files

--- Phase_1/processSelectToExcel.py
+++ Phase_1/processSelectToExcel.py
@@ -94,7 +94,7 @@ def process_files(folder_path, output_excel_file):
             print(f)
 
 if __name__ == "__main__":
-    folder_path = r'text_files\2020-06'
+    folder_path = r'text_files\2021-01'
     #folder_path = 'testing_files'
     output_excel_file = 'select_race_data.xlsx'
     process_files(folder_path, output_excel_file)
